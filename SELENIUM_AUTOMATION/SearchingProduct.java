f8b535c7b56544c8b6839dbebe2018492a01192b
SaiSivaSanjay <93181342+SaiSivaSanjay@users.noreply.github.com>
2022-11-26 18:10:40 +0530

    Update SearchingProduct.java

--- SELENIUM_AUTOMATION/SearchingProduct.java
+++ SELENIUM_AUTOMATION/SearchingProduct.java
@@ -1,1 +1,27 @@
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Seleniumdemo11 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.amazon.com/");
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"nav-main\"]/div[1]/div/div/div[3]/span[1]/span/input")).click();
+		WebElement findElement = driver.findElement(By.id("twotabsearchtextbox"));
+		findElement.click();
+		findElement.sendKeys("Asus rog 5");
