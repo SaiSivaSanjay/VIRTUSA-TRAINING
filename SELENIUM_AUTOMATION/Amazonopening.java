84b407b89f74e1b30cd6ee1fa0d49523736808f0
SaiSivaSanjay <93181342+SaiSivaSanjay@users.noreply.github.com>
2022-11-26 18:09:23 +0530

    Update Amazonopening.java

--- SELENIUM_AUTOMATION/Amazonopening.java
+++ SELENIUM_AUTOMATION/Amazonopening.java
@@ -1,1 +1,22 @@
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Seleniumdemo11 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.amazon.com/");
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
